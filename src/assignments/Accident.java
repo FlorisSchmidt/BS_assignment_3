c6e76e025b21b35c033accf64a96eab02658dde2
floris.schmidt <floris.schmidt@hotmail.com>
2020-02-03 12:41:03 +0100

    Added serveOutside

--- src/assignments/Accident.java
+++ src/assignments/Accident.java
@@ -11,18 +11,15 @@ public class Accident {
     private double serviceTime;
     private double xLocation;
     private double yLocation;
-    private int region;
 
-    public Accident(double arrivalTime, double xLocation, double yLocation, int region) {
+    public Accident(double arrivalTime, double xLocation, double yLocation) {
         //Record arrival time when creating a new customer
         this.arrivalTime = arrivalTime;
         this.startTime = Double.NaN;
-        this.completionTime = Double.NaN;
         this.waitTime = Double.NaN;
         this.serviceTime = Double.NaN;
         this.xLocation = xLocation;
         this.yLocation = yLocation;
-        this.region = region;
     }
     
     public double[] getLocation() {
